4856c0a2503292344b326abde111db0aa53a8691
Rodolfo Diaz <rodolfodzr@gmail.com>
2020-11-09 20:59:21 -0500

    adding features to the index.js

--- index.js
+++ index.js
@@ -71,7 +71,6 @@ function generateMD(answers){
     var profile=("https://github.com/"+answers.github)
     return `
 # ${answers.title}
-## Licensing 
 ${answers.badge ==="Apache" ? "Apache"+""+'<br>'+""+"[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)" : answers.badge ==="MIT" ? "MIT"+""+'<br>'+""+"[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)" : answers.badge==="IBM" ? "IBM"+""+'<br>'+""+"[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)" : "Perl"+""+'<br>'+""+"[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)"}
 ## Description 
      ${answers.description}
